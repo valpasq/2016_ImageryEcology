6e94e912327d27c46f62d37b24dd312737fb8bde
Valerie <valpasq@bu.edu>
2016-07-19 19:14:04 -0400

    added script to run basic plots

---
+++ Plot_code/Scripts/run_plots.m
@@ -0,0 +1,43 @@
+% Run plots
+%function [] = pixel_plot_generator_auto(WRS, savedir, N_row, N_col, geplot, gedate, gedate2, gedate3, gedate4, plotout)
+
+%savedir = '/usr3/graduate/valpasq/Documents/2016_ImageryEcology/figures/abrupt_change/';
+%pixel_plot_generator_auto('p012r031', savedir, 1422, 4384, 'on', '06/12/2001', '10/27/2002', '09/27/2014', [], 'combined')
+%pixel_plot_generator_auto('p012r031', savedir, 2040, 4983, 'on', '04/13/2002', '08/18/2003', '08/24/2013', [], 'combined')
+%pixel_plot_generator_auto('p012r031', savedir, 4156, 5933, 'on', '03/09/1995', '07/28/2007', '08/30/2012', [], 'combined')
+
+%savedir = '/usr3/graduate/valpasq/Documents/2016_ImageryEcology/figures/disturbance-recovery/';
+%pixel_plot_generator_auto('p012r031', savedir, 2636, 4551, 'on', '04/02/1995', '07/06/2003', '07/04/2014', [], 'combined')
+%pixel_plot_generator_auto('p012r031', savedir, 996, 2224, 'on', '03/28/1995', '12/31/2000', '09/20/12', [], 'combined')
+
+%savedir = '/usr3/graduate/valpasq/Documents/2016_ImageryEcology/figures/gradual_change/';
+%pixel_plot_generator_auto('p012r031', savedir, 1859, 4629, 'on', '04/02/1995', '07/26/2004', '09/27/2014', [], 'combined')
+%pixel_plot_generator_auto('p012r031', savedir, 1664, 4419, 'on', '05/06/1996', '10/27/2002', '08/24/2013', [], 'combined')
+%pixel_plot_generator_auto('p011r031', savedir, 2994, 2882, 'on', '03/09/1995', '07/28/2007', '08/30/2012', [], 'combined')
+
+savedir = '/usr3/graduate/valpasq/Documents/2016_ImageryEcology/figures/forest/';
+pixel_plot_generator_auto('p007r059', savedir, 5927, 4375, 'off', [], [], [], [], 'separate')
+pixel_plot_generator_auto('p006r060', savedir, 4415, 2034, 'off', [], [], [], [], 'separate')
+pixel_plot_generator_auto('p125r053', savedir, 5844, 5225, 'off', [], [], [], [], 'separate')
+
+pixel_plot_generator_auto('p011r031', savedir, 3106, 3070, 'off', [], [], [], [], 'separate')
+pixel_plot_generator_auto('p012r031', savedir, 1770, 3752, 'off', [], [], [], [], 'separate')
+pixel_plot_generator_auto('p013r030', savedir, 5868, 1613, 'off', [], [], [], [], 'separate')
+
+pixel_plot_generator_auto('p013r030', savedir, 5816, 1778, 'off', [], [], [], [], 'separate')
+pixel_plot_generator_auto('p012r031', savedir, 1772, 3775, 'off', [], [], [], [], 'separate')
+pixel_plot_generator_auto('p013r030', savedir, 5814, 4240, 'off', [], [], [], [], 'separate')
+
+pixel_plot_generator_auto('p189r017', savedir, 2934, 4375, 'off', [], [], [], [], 'separate')
+pixel_plot_generator_auto('p189r017', savedir, 2812, 4349, 'off', [], [], [], [], 'separate')
+pixel_plot_generator_auto('p189r017', savedir, 2775, 4367, 'off', [], [], [], [], 'separate')
+
+savedir = '/usr3/graduate/valpasq/Documents/2016_ImageryEcology/figures/wetland_gradient/';
+pixel_plot_generator_auto('p012r031', savedir, 1801, 3742, 'off', [], [], [], [], 'separate')
+pixel_plot_generator_auto('p012r031', savedir, 1764, 3788, 'off', [], [], [], [], 'separate')
+pixel_plot_generator_auto('p012r031', savedir, 3052, 4858, 'off', [], [], [], [], 'separate')
+
+savedir = '/usr3/graduate/valpasq/Documents/2016_ImageryEcology/figures/urban_gradient/';
+pixel_plot_generator_auto('p012r031', savedir, 1423, 4377, 'off', [], [], [], [], 'separate')
+pixel_plot_generator_auto('p012r031', savedir, 1433, 4361, 'off', [], [], [], [], 'separate')
+pixel_plot_generator_auto('p012r031', savedir, 1842, 3711, 'off', [], [], [], [], 'separate')
